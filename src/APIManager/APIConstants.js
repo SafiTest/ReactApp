b6da947a818a01cb21523336f15ddf3c03ae1c09
SafiTest <106875646+SafiTest@users.noreply.github.com>
2022-06-28 17:08:54 +0530

    Update APIConstants.js

--- src/APIManager/APIConstants.js
+++ src/APIManager/APIConstants.js
@@ -1,8 +1,7 @@
 export const LoginUser = "/login";
 export const SignupUser = "/users";
-export const GetMembershipList = "/users/get";
+export const GetMembershipList = "/table";
 export const CreateOrder="/createOrder";
 export const ExcelfileRead="/excelFileRead";
 export const Alluser="/allUser";
 export const ForgotPassword="/forgotPassword"
-
